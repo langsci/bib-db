d5a892c44d306b764459ad3fde519d761ec7364c
timmli <lichte@phil.hhu.de>
2015-11-14 10:33:56 +0100

    fixed crossref issue; added sources

--- add_bibfile.sh
+++ add_bibfile.sh
@@ -12,11 +12,14 @@ if [[ -f $1 ]]; then
 	FILENAME="${FILE%%.*}"
 	## replace non-ascii symbols by TeX macros 
 	biber --tool --output_encoding=ascii $1
+	## first run with BibTool to resolve crossrefs
+	bibtool -r bibtool-config.rsc -i "$FILENAME"_bibertool.bib -o "$FILENAME"_bibertool.bib 2> bibtool-1st.log
+	>&2 echo "$(<bibtool-1st.log)"
 	## remove keys  
 	perl -pe 's/^(@.*?{).*$/\1,/' "$FILENAME"_bibertool.bib > "$FILENAME"_bibertool_keyless.bib
-	## let BibTool do the rest 
-	bibtool -r bibtool-config.rsc -i langsci.bib "$FILENAME"_bibertool_keyless.bib -o langsci.bib 2> bibtool.log
-	>&2 echo "$(<bibtool.log)"
+	## second run with BibTool to merge into langsci.bib 
+	bibtool -r bibtool-config.rsc -i langsci.bib "$FILENAME"_bibertool_keyless.bib -o langsci.bib 2> bibtool-2nd.log
+	>&2 echo "$(<bibtool-2nd.log)"
 	## remove temporary files
 	rm "$FILENAME"_bibertool.bib "$FILENAME"_bibertool_keyless.bib
 else
