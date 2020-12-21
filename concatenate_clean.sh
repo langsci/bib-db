48bfcaaf25760239170d480d832a9888aefd15d6
mstelling <marten.stelling@uni-goettingen.de>
2020-12-21 20:24:43 +0100

    maybe localbibs.bib now is mostly unique items?

--- concatenate_clean.sh
+++ concatenate_clean.sh
@@ -3,6 +3,7 @@
 #remove comments. This messes up any inline comments at this point.
 sed -i '/^\s*%/d' sources/noerrors/*.bib
 sed -i '/^\s*$/d' sources/noerrors/*.bib
+sed -i '/\^M/d' sources/noerrors/*.bib
 
 # concatenate, hopefully deduplicate
 bibtool -s -d sources/noerrors/*.bib > localbibs.bib
